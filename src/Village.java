44d89a975cf56182ffc01d86dcd322a1d0e19083
Daphne Aubert <daaubert@emi.u-bordeaux.fr>
2022-12-15 08:49:58 +0100

    add new message random

--- src/Village.java
+++ src/Village.java
@@ -1,15 +1,19 @@
+import io.jbotsim.core.Message;
 import io.jbotsim.core.Node;
 import io.jbotsim.ui.icons.Icons;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Village extends Node {
     String name;
     List<Node> villages;
+    List<Message> mailbox;
 
     public Village(String name, List<Node> villages) {
         this.name = name;
         this.villages = villages;
+        mailbox = new ArrayList<>();
     }
 
     @Override
@@ -21,7 +25,18 @@ public class Village extends Node {
     }
 
     @Override
+    public void onClock() {
+        if (Math.random() < 0.005) {
+            newMessage();
+        }
+    }
+
+    public void newMessage(){
+        mailbox.add(new Message("cc c nou"));
+    }
+
+    @Override
     public String toString() {
-        return super.toString() + " " + name;
+        return super.toString() + " " + name + ", msg : " + mailbox.size();
     }
 }
