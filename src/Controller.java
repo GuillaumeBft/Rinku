1020345f1ab0274078b0b43dadf5f7eba5df6889
gbienfait <guillaume.bienfait@etu.u-bordeaux.fr>
2022-12-20 11:20:18 +0100

    ENHANCED switch

--- src/Controller.java
+++ src/Controller.java
@@ -56,15 +56,9 @@ public class Controller implements CommandListener {
     public static void computeItinerary(List<Node> nodes, String selectedAlgo){
         Algorithm algorithm = new Algorithm(nodes);
         switch (selectedAlgo) {
-            case RANDOM_INSERT:
-                itinerary = algorithm.randomInsertion();
-                break;
-            case BRUTEFORCE:
-                itinerary = algorithm.bruteForce();
-                break;
-            default:
-                itinerary = algorithm.noAlgo();
-                break;
+            case RANDOM_INSERT -> itinerary = algorithm.randomInsertion();
+            case BRUTEFORCE -> itinerary = algorithm.bruteForce();
+            default -> itinerary = algorithm.noAlgo();
         }
         System.out.println("Distance of the itinerary made by " + selectedAlgo + " is " + Algorithm.getDistance(itinerary.getSteps()));
     }
