c4494a3e9c7cddf200534261c86bcb0ad0548080
gbienfait <guillaume.bienfait@etu.u-bordeaux.fr>
2022-12-20 10:38:11 +0100

    Select the algo you want for compute the path of the robot

--- src/Controller.java
+++ src/Controller.java
@@ -2,30 +2,36 @@ import io.jbotsim.core.Node;
 import io.jbotsim.core.Point;
 import io.jbotsim.core.Topology;
 import io.jbotsim.core.event.CommandListener;
-import io.jbotsim.ui.JTopology;
 import io.jbotsim.ui.JViewer;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Controller implements CommandListener {
     public static final String START = "Start execution";
+    public static final String BRUTEFORCE = "Set Bruteforce algo";
+    public static final String RANDOM_INSERT = "Set Random Insertion algo";
     private Topology topology;
     static Map<Point, Node> villages;
     static Itinerary itinerary;
+
+    private String selectedAlgo;
 
     public Controller() {
         topology = new Topology();
         topology.setDefaultNodeModel(Village.class);
         topology.setTimeUnit(100);
 
+        selectedAlgo = "";
+
         villages = new HashMap<>();
-        addDataVillages();
+        addVillages();
 
         new JViewer(topology);
         topology.addCommandListener(this);
+        topology.addCommand(RANDOM_INSERT);
+        topology.addCommand(BRUTEFORCE);
     }
 
     @Override
@@ -36,19 +42,34 @@ public class Controller implements CommandListener {
                     villages.put(n.getLocation(), n);
                 }
             }
-            computeItinerary(topology.getNodes());
-            addDataRobots();
+            computeItinerary(topology.getNodes(), selectedAlgo);
+            addRobots();
+        } else if (command.equals(BRUTEFORCE)) {
+            selectedAlgo = BRUTEFORCE;
+            System.out.println("Bruteforce optimal selection algorithm has been selected for the compute of the robot path.");
+        } else if (command.equals(RANDOM_INSERT)) {
+            selectedAlgo = RANDOM_INSERT;
+            System.out.println("Random insertion algorithm has been selected for the compute of the robot path.");
         }
     }
 
-    public static void computeItinerary(List<Node> nodes){
+    public static void computeItinerary(List<Node> nodes, String selectedAlgo){
         Algorithm algorithm = new Algorithm(nodes);
-        algorithm.bruteForce();
-        itinerary = algorithm.bruteForce();
-        System.out.println("Distance of the itinerary is " + Algorithm.getDistance(itinerary.getSteps()));
+        switch (selectedAlgo) {
+            case RANDOM_INSERT:
+                itinerary = algorithm.randomInsertion();
+                break;
+            case BRUTEFORCE:
+                itinerary = algorithm.bruteForce();
+                break;
+            default:
+                itinerary = algorithm.noAlgo();
+                break;
+        }
+        System.out.println("Distance of the itinerary made by " + selectedAlgo + " is " + Algorithm.getDistance(itinerary.getSteps()));
     }
 
-    public void addDataVillages(){
+    public void addVillages(){
         topology.addNode(20, 200, new Village("Signy-Le-Petit"));
         topology.addNode(20, 70, new Village("Cussac"));
         topology.addNode(100, 200, new Village("Croissy"));
@@ -56,7 +77,7 @@ public class Controller implements CommandListener {
         topology.addNode(250, 250, new Village("Croatie"));
     }
 
-    public void addDataRobots(){
+    public void addRobots(){
         topology.addNode(100, 100, new Robot(itinerary));
         //topology.addNode(100, 100, new Robot(new Itinerary(itinerary.getSteps(), itinerary.getStart() + itinerary.getSize()/2)));
     }
